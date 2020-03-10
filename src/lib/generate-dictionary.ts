bb4b90089789139ffc6dd717e07670dfbe0126ad
Johann <mail@johannroehl.de>
2020-03-10 11:35:10 +0100

    fix(translations): Updated translations (#8)

--- src/lib/generate-dictionary.ts
+++ src/lib/generate-dictionary.ts
@@ -1,7 +1,11 @@
+import { cloneDeep } from 'lodash';
+
 import estateCommon from '../translations';
 import { Mapping } from '../classes/portals/Estate';
 import { AvailableLanguages } from '../types';
 import { generateEstatePropertyKeys } from './get-keys';
+
+const safeEstateCommon = cloneDeep(estateCommon);
 
 const objectify = (array: string[], mapping: Mapping = {}) =>
   array.reduce((red, key) => {
@@ -14,6 +18,6 @@ const objectify = (array: string[], mapping: Mapping = {}) =>
 
 export const generateDictionary = (language: AvailableLanguages): Mapping => {
   const commonKeys = generateEstatePropertyKeys();
-  const mapping = language ? estateCommon[language] : undefined;
+  const mapping = language ? safeEstateCommon[language] : undefined;
   return objectify(commonKeys, mapping);
 };
